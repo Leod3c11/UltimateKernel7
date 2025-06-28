3a37c3445a6a6bd727eef1396bb0f7ad12a9f03e
Leod3c11 <133391551+Leod3c11@users.noreply.github.com>
2025-06-27 22:51:55 -0300

    Add files via upload

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -20,6 +20,8 @@
 #include <linux/sti/abc_common.h>
 #endif
 
+#include <linux/gaming_control.h>
+
 #if defined(CONFIG_KUNIT)
 #define __visible_for_testing
 #else
@@ -4975,6 +4977,9 @@ static void sec_bat_check_full_capacity(struct sec_battery_info *battery)
 {
 	int rechg_capacity = battery->batt_full_capacity - 2;
 
+	if(battery_idle_gaming() && battery->capacity >= 20)
+		goto warn;
+
 	if (battery->batt_full_capacity >= 100 || battery->batt_full_capacity <= 0 ||
 		battery->status == POWER_SUPPLY_STATUS_DISCHARGING) {
 		if (battery->misc_event & BATT_MISC_EVENT_FULL_CAPACITY) {
@@ -4991,6 +4996,7 @@ static void sec_bat_check_full_capacity(struct sec_battery_info *battery)
 			sec_bat_recov_full_capacity(battery);
 		}
 	} else if (battery->capacity >= battery->batt_full_capacity) {
+warn:
 		pr_info("%s : stop charging(%d, %d)\n", __func__, battery->capacity, battery->batt_full_capacity);
 
 		sec_bat_set_misc_event(battery, BATT_MISC_EVENT_FULL_CAPACITY,
